9f80680496b2c76c36ceabb95533785c282ac76b
Derek Watson <derek.watson92@gmail.com>
2020-06-16 19:21:10 -0700

    added highscore js functionality & accompanying HTML

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let questions = [
+// Questions Data
+const questions = [
   {
     question: "Commonly used data types DO NOT include:",
     options: [
@@ -29,17 +30,78 @@ let questions = [
   },
 ];
 
-let clockDisplay = document.querySelector(".clock");
-let startCard = document.querySelector(".start-card");
-let startBtn = document.querySelector(".start-btn");
-let questionCard = document.querySelector(".question-card");
-let endCard = document.querySelector(".end-card");
+// Clock and Nav
+const clockDisplay = document.querySelector(".clock");
+const highscoresNav = document.querySelector(".see-highscores");
 
-let clock = 200;
-let ticker;
+// Start Card
+const startCard = document.querySelector(".start-card");
+const startBtn = document.querySelector(".start-btn");
+
+// Question Display Card
+const questionCard = document.querySelector(".question-card");
+
+// Submit Highscore Card
+const endCard = document.querySelector(".end-card");
+const endForm = document.querySelector(".score-form");
+const initialsInput = document.querySelector("#initials");
+
+// Highscores Card
+const scoreCard = document.querySelector(".score-card");
+const scoreTableBody = document.querySelector(".scores-body");
+const backBtn = document.querySelector(".back-btn");
+const clearBtn = document.querySelector(".clear-btn");
+
+// Global Variables
+let highscores;
+let clock = 100;
+// Set clock on page load
 clockDisplay.innerHTML = clock;
+let ticker;
+let questionIndex = 0;
 
-let questionNumber = 0;
+function fetchHighscores() {
+  let localHighscores = localStorage.getItem("highscores");
+  if (localHighscores) {
+    highscores = JSON.parse(localHighscores);
+  } else {
+    highscores = [];
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+  }
+}
+
+function updateHighscores(newHighscore) {
+  highscores.push(newHighscore);
+  highscores = highscores.sort((a, b) => b.score - a.score);
+  localStorage.setItem("highscores", JSON.stringify(highscores));
+}
+
+function clearHighscores() {
+  highscores = [];
+  localStorage.setItem("highscores", JSON.stringify(highscores));
+  displayHighscores();
+}
+
+function displayHighscores() {
+  scoreTableBody.innerHTML = "";
+  highscores.forEach((item, index) => {
+    const row = document.createElement("tr");
+
+    const rankTh = document.createElement("th");
+    rankTh.textContent = index + 1;
+    row.appendChild(rankTh);
+
+    const initialsTd = document.createElement("td");
+    initialsTd.textContent = item.initials;
+    row.appendChild(initialsTd);
+
+    const scoreTd = document.createElement("td");
+    scoreTd.textContent = item.score;
+    row.appendChild(scoreTd);
+
+    scoreTableBody.appendChild(row);
+  });
+}
 
 function displayElement(element, on) {
   if (on) {
@@ -51,10 +113,25 @@ function displayElement(element, on) {
   }
 }
 
+function hideAllCards() {
+  displayElement(startCard, false);
+  displayElement(questionCard, false);
+  displayElement(endCard, false);
+  displayElement(scoreCard, false);
+}
+
 function endGame() {
   displayElement(questionCard, false);
   displayElement(endCard, true);
+  clearInterval(ticker);
   endCard.querySelector(".final-score").textContent = clock;
+}
+
+function abortGame() {
+  clearInterval(ticker);
+  questionIndex = 0;
+  clock = 100;
+  clockDisplay.innerHTML = clock;
 }
 
 function startTimer() {
@@ -66,19 +143,18 @@ function startTimer() {
       }
       clockDisplay.innerHTML = clock;
     } else {
-      clearInterval(ticker);
       endGame();
     }
   }, 1000);
 }
 
 function flashResult(message) {
-  let resultDisplay = questionCard.querySelector(".result");
+  const resultDisplay = questionCard.querySelector(".q-result");
   displayElement(resultDisplay, true);
   resultDisplay.lastElementChild.innerHTML = message[0];
   resultDisplay.lastElementChild.style.color = message[1];
   clockDisplay.style.color = message[1];
-  let flash = setTimeout(() => {
+  const flash = setTimeout(() => {
     displayElement(resultDisplay, false);
     clockDisplay.style.color = "inherit";
     clearTimeout(flash);
@@ -86,23 +162,23 @@ function flashResult(message) {
 }
 
 function handleSelection(e) {
-  let correct = e.target.dataset.correct === "true";
-  let message = correct
+  const correct = e.target.dataset.correct === "true";
+  const message = correct
     ? ["Correct!", "forestgreen"]
     : ["Incorrect!", "firebrick"];
   flashResult(message);
-  if (e.target.dataset.correct === "false") {
+  if (!correct) {
     clock = clock - 10;
+    clockDisplay.innerHTML = clock;
   }
   advanceGame();
 }
 
 function advanceGame() {
-  if (questionNumber < questions.length - 1) {
-    questionNumber++;
-    displayQuestion(questions[questionNumber]);
+  if (questionIndex < questions.length - 1) {
+    questionIndex++;
+    displayQuestion(questions[questionIndex]);
   } else {
-    clearInterval(ticker);
     endGame();
   }
 }
@@ -118,11 +194,41 @@ function displayQuestion(q) {
 
 function startGame() {
   startTimer();
-  displayQuestion(questions[questionNumber]);
+  displayQuestion(questions[questionIndex]);
 }
+
+function init() {
+  fetchHighscores();
+  displayElement(startCard, true);
+}
+
+highscoresNav.addEventListener("click", () => {
+  abortGame();
+  hideAllCards();
+  displayHighscores();
+  displayElement(scoreCard, true);
+});
+
+clearBtn.addEventListener("click", clearHighscores);
+
+backBtn.addEventListener("click", () => {
+  abortGame();
+  hideAllCards();
+  displayElement(startCard, true);
+});
 
 startBtn.addEventListener("click", () => {
   displayElement(startCard, false);
   displayElement(questionCard, true);
   startGame();
 });
+
+endForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  updateHighscores({ initials: initialsInput.value, score: clock });
+  displayElement(endCard, false);
+  displayElement(scoreCard, true);
+  displayHighscores();
+});
+
+init();
